58dae59a24bdd34d8e4086cec9492951edbb12f6
Salma Hanifah <salma.hanifah11@ui.ac.id>
2022-09-14 10:03:47 +0700

    TUGAS 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang' : data_katalog,
+        'name' : 'Salma Hanifah',
+        'studentID' : '2106751594'
+    }
+
+    return render(request, "katalog.html", context)
