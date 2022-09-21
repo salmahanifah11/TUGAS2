f3ccd038e198ccb1838abdc7df5079f229953855
Salma Hanifah <salma.hanifah11@ui.ac.id>
2022-09-21 11:45:37 +0700

    TUGAS 3

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from mywatchlist.models import watchlistItem
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_watchlist(request):
@@ -9,3 +11,20 @@ def show_watchlist(request):
         'nama' : "Salma Hanifah"
     }
     return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = watchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = watchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def show_xml_by_id(request, id):
+    data = watchlistItem.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = watchlistItem.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
