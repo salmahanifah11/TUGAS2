870913de1513c242cab317c2bf7cc5dedf197f4f
Salma Hanifah <salma.hanifah11@ui.ac.id>
2022-09-22 07:09:30 +0700

    TUGAS 3

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,4 +1,7 @@
-from django.test import TestCase
-from django.urls import resolve
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class unitTest(SimpleTestCase):
+    def test_url_exist(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
