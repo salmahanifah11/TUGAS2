1bee12ba2ee6fdf2efbb4adf995bc70b68f9d3a0
Salma Hanifah <salma.hanifah11@ui.ac.id>
2022-09-22 10:48:01 +0700

    TUGAS 3

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -8,7 +8,6 @@ from mywatchlist.views import show_json_by_id
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_watchlist, name='show_watchlist'),
     path('html/', show_watchlist, name='show_watchlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
