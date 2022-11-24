ef8eb365c058b54fec75eff6f4e5ed39346887b5
Salma Hanifah <salma.hanifah11@ui.ac.id>
2022-11-24 11:36:50 +0700

    init

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class watchlistItem(models.Model):
-    item_watched = models.CharField(max_length=255, default='')
+    item_watched = models.BooleanField()
     item_title = models.CharField(max_length=255, default='')
     rating = models.IntegerField(default='')
     release_date = models.TextField(default='')
